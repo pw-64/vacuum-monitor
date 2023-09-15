0a48ee54b7602d21e8150bf927e5795d5295d914
pw-64 <64085841+pw-64@users.noreply.github.com>
2023-09-15 17:03:28 +0100

    Update vacuum_monitor.ino
    5 -> 10 LEDs
    Switched from potentiometer to actual vacuum sensor
    Simplified LED control using bools and updater functions

--- vacuum_monitor.ino
+++ vacuum_monitor.ino
@@ -2,47 +2,106 @@ const int delayTime = 1500; // how long to wait between actions, 1500 (1.5 secs)
 
 const bool debugOutput = true; // turn output messages to the serial monitor on/off
 
+// true == HIGH
+// false == LOW
+
 // PINS
-const int vacuumSensorPin = A0; // potentiometer input to simulate vacuum reading
+const int vacuumSensorPin = A0;
 // vacuum level bar chart
-const int redLED1 = 29;
-const int redLED2 = 30;
-const int redLED3 = 31;
-const int redLED4 = 32;
-const int redLED5 = 33;
+const int led_1_pin = 12;
+const int led_2_pin = 11;
+const int led_3_pin = 10;
+const int led_4_pin = 9;
+const int led_5_pin = 8;
+const int led_6_pin = 7;
+const int led_7_pin = 6;
+const int led_8_pin = 5;
+const int led_9_pin = 4;
+const int led_10_pin = 3;
+
+bool led_1_on = LOW;
+bool led_2_on = LOW;
+bool led_3_on = LOW;
+bool led_4_on = LOW;
+bool led_5_on = LOW;
+bool led_6_on = LOW;
+bool led_7_on = LOW;
+bool led_8_on = LOW;
+bool led_9_on = LOW;
+bool led_10_on = LOW;
 
 int vacuumSensorValue; // create a blank variable to hold the value from the Vacuum Sensor Pin that can be assigned a value later
 
-void initPinOutput(int pin, bool startState) {pinMode(pin, OUTPUT); digitalWrite(pin, startState);} // compact way to set a pin as an output and its starting state (ONLY FOR DIGITAL OUTPUT)
 bool inRange(int minimum, int value, int maximum) {return ((value >= minimum) && (value < maximum));} // return a boolean if a value is within a range
 void print(char message[]) {if (debugOutput == true) {Serial.println(message);}} // print a message to the serial monitor
 
+void SetAllLEDs(bool state) {
+  led_1_on = state;
+  led_2_on = state;
+  led_3_on = state;
+  led_4_on = state;
+  led_5_on = state;
+  led_6_on = state;
+  led_7_on = state;
+  led_8_on = state;
+  led_9_on = state;
+  led_10_on = state;
+  UpdateLEDs();
+}
+
+void UpdateLEDs() {
+  digitalWrite(led_1_pin, led_1_on);
+  digitalWrite(led_2_pin, led_2_on);
+  digitalWrite(led_3_pin, led_3_on);
+  digitalWrite(led_4_pin, led_4_on);
+  digitalWrite(led_5_pin, led_5_on);
+  digitalWrite(led_6_pin, led_6_on);
+  digitalWrite(led_7_pin, led_7_on);
+  digitalWrite(led_8_pin, led_8_on);
+  digitalWrite(led_9_pin, led_9_on);
+  digitalWrite(led_10_pin, led_10_on);
+}
+
 // run this first, once, when the arduino recieves power or the reset button is pressed
 void setup() {
-  Serial.begin(9600); delay(delayTime); print("Starting ...");  
-  
-  delay(delayTime); print("  Initialising Vacuum Level LEDs");
-  initPinOutput(redLED1, LOW);
-  initPinOutput(redLED2, LOW);
-  initPinOutput(redLED3, LOW);
-  initPinOutput(redLED4, LOW);
-  initPinOutput(redLED5, LOW);
+  Serial.begin(9600);
   delay(delayTime);
-  digitalWrite(redLED1, HIGH); digitalWrite(redLED2, HIGH); digitalWrite(redLED3, HIGH); digitalWrite(redLED4, HIGH); digitalWrite(redLED5, HIGH);
+  print("Starting");
   delay(delayTime);
-  digitalWrite(redLED1, LOW); digitalWrite(redLED2, LOW); digitalWrite(redLED3, LOW); digitalWrite(redLED4, LOW); digitalWrite(redLED5, LOW);
-  
-  delay(delayTime); print("  Initialising Vacuum Sensor Input");
+  print("Initialising Vacuum Level LEDs");
+  pinMode(led_1_pin, OUTPUT);
+  pinMode(led_2_pin, OUTPUT);
+  pinMode(led_3_pin, OUTPUT);
+  pinMode(led_4_pin, OUTPUT);
+  pinMode(led_5_pin, OUTPUT);
+  pinMode(led_6_pin, OUTPUT);
+  pinMode(led_7_pin, OUTPUT);
+  pinMode(led_8_pin, OUTPUT);
+  pinMode(led_9_pin, OUTPUT);
+  pinMode(led_10_pin, OUTPUT);
+  delay(delayTime);
+  SetAllLEDs(HIGH);
+  delay(delayTime);
+  SetAllLEDs(LOW);
+  delay(delayTime);
+  print("Initialising Vacuum Sensor Input");
   pinMode(vacuumSensorPin, INPUT);
   delay(delayTime);
 }
 
 void loop() {
   vacuumSensorValue = analogRead(vacuumSensorPin);
-  if (inRange(0, vacuumSensorValue, 200)) {digitalWrite(redLED1, HIGH); digitalWrite(redLED2, LOW); digitalWrite(redLED3, LOW); digitalWrite(redLED4, LOW); digitalWrite(redLED5, LOW);}
-  if (inRange(200, vacuumSensorValue, 400)) {digitalWrite(redLED1, HIGH); digitalWrite(redLED2, HIGH); digitalWrite(redLED3, LOW); digitalWrite(redLED4, LOW); digitalWrite(redLED5, LOW);}
-  if (inRange(400, vacuumSensorValue, 600)) {digitalWrite(redLED1, HIGH); digitalWrite(redLED2, HIGH); digitalWrite(redLED3, HIGH); digitalWrite(redLED4, LOW); digitalWrite(redLED5, LOW);}
-  if (inRange(600, vacuumSensorValue, 800)) {digitalWrite(redLED1, HIGH); digitalWrite(redLED2, HIGH); digitalWrite(redLED3, HIGH); digitalWrite(redLED4, HIGH); digitalWrite(redLED5, LOW);}
-  if (inRange(800, vacuumSensorValue, 1023)) {digitalWrite(redLED1, HIGH); digitalWrite(redLED2, HIGH); digitalWrite(redLED3, HIGH); digitalWrite(redLED4, HIGH); digitalWrite(redLED5, HIGH);}
+  print(vacuumSensorValue);
+  if (inRange(0,   vacuumSensorValue, 100))  {led_1_on = true;}
+  if (inRange(100, vacuumSensorValue, 200))  {led_2_on = true;}
+  if (inRange(200, vacuumSensorValue, 300))  {led_3_on = true;}
+  if (inRange(300, vacuumSensorValue, 400))  {led_4_on = true;}
+  if (inRange(400, vacuumSensorValue, 500))  {led_5_on = true;}
+  if (inRange(500, vacuumSensorValue, 600))  {led_6_on = true;}
+  if (inRange(600, vacuumSensorValue, 700))  {led_7_on = true;}
+  if (inRange(700, vacuumSensorValue, 800))  {led_8_on = true;}
+  if (inRange(800, vacuumSensorValue, 900))  {led_9_on = true;}
+  if (inRange(900, vacuumSensorValue, 1023)) {led_10_on = true;}
+  UpdateLEDs();
   delay(100);
 }
